23948f802b330a94d897df0a7b89e2178243c164
Taqi Hamoda <taqihisham@gmail.com>
2023-09-27 21:48:20 -0400

    Started working on it again

--- python/TODO.md
+++ python/TODO.md
@@ -3,11 +3,11 @@
 ## TODOs
 - [ ] Port CTCR
     - [ ] Figure out how to visualize a CTCR in MatPlotLib
-    - [ ] Port over the math
+    - [x] Port over the math
     - [ ] Modularize and clean up the code
 - [ ] Port TDCR
     - [ ] Figure out how to visualize a TDCR in MatPlotLib
-    - [ ] Port over the math
+    - [x] Port over the math
     - [ ] Modularize and clean up the code
 
 ## Resources
--- python/robotindependentmapping.py
+++ python/robotindependentmapping.py
@@ -25,7 +25,8 @@ def robotindependentmapping(kappa: np.ndarray[np.double], phi: np.ndarray[np.dou
     Returns
     -------
     g : ndarray
-        (n,16) backbone curve with n 4x4 transformation matrices reshaped into 1x16 vector (columnwise)
+        (mx16) backbone curve with m 4x4 transformation matrices, where m is
+            total number of points, reshaped into 1x16 vector (columnwise)
 
     Author: Jessica Burgner-Kahrs <jbk@cs.toronto.edu>
     Date: 2022/02/16
@@ -37,13 +38,13 @@ def robotindependentmapping(kappa: np.ndarray[np.double], phi: np.ndarray[np.dou
         raise ValueError("Dimension mismatch.")
     
     numseg = kappa.shape[0]
-    if len(ptsperseg) == 1 and numseg > 1: #same number of points per segment
-        ptsperseg = np.tile(ptsperseg, (numseg, ))
+    if ptsperseg.size == 1 and numseg > 1: # If same number of points per segment
+        ptsperseg = np.tile(ptsperseg, numseg)  # Create an array that is numseg long with the num points repeated
 
-    g = np.zeros((np.sum(ptsperseg), 16))
-    T_base = np.eye(4)
+    g = np.zeros((np.sum(ptsperseg), 16))  # Stores the transformation matrices of all the points in all the segments as rows
+
+    T_base = np.eye(4)  # base starts off as identity
     for i in range(numseg):
-        T = np.zeros((ptsperseg[i], 16))
         c_p = np.cos(phi[i])
         s_p = np.sin(phi[i])
 
@@ -51,23 +52,18 @@ def robotindependentmapping(kappa: np.ndarray[np.double], phi: np.ndarray[np.dou
             c_ks = np.cos(kappa[i] * j * (ell[i]/ptsperseg[i]))
             s_ks = np.sin(kappa[i] * j * (ell[i]/ptsperseg[i]))
 
-            if kappa[i] != 0:
-                    T_temp = np.array([[c_p*c_p*(c_ks-1) + 1, s_p*c_p*(c_ks-1), -c_p*s_ks, 0],
-                                       [s_p*c_p*(c_ks-1), c_p*c_p*(1-c_ks) + c_ks, -s_p*s_ks, 0],
-                                       [c_p*s_ks, s_p*s_ks, c_ks, 0],
-                                       [(c_p*(1-c_ks))/kappa[i], (s_p*(1-c_ks))/kappa[i], s_ks/kappa[i], 1]])
-            else: #kappa=0 ->otherwise division by zero
-                    T_temp = np.array([[c_p*c_p*(c_ks-1) + 1, s_p*c_p*(c_ks-1), -c_p*s_ks, 0],
-                                       [s_p*c_p*(c_ks-1), c_p*c_p*(1-c_ks) + c_ks, -s_p*s_ks, 0],
-                                       [c_p*s_ks, s_p*s_ks, c_ks, 0],
-                                       [0, 0, j*(ell[i]/ptsperseg[i]), 1]])
-            T[j, :] = (T_base @ T_temp.reshape(4, 4)).reshape(1, 16)
+            T_temp = np.array([
+                [c_p*c_p*(c_ks-1) + 1, s_p*c_p*(c_ks-1), -c_p*s_ks, 0],
+                [s_p*c_p*(c_ks-1), c_p*c_p*(1-c_ks) + c_ks, -s_p*s_ks, 0],
+                [c_p*s_ks, s_p*s_ks, c_ks, 0],
+                [0, 0, j*(ell[i]/ptsperseg[i]), 1]
+            ])
 
-        if i == 0:
-            g[:ptsperseg[i], :] = T
-        else:
-            g[np.sum(ptsperseg[:i]):np.sum(ptsperseg[:i + 1]), :] = T
+            if kappa[i] != 0:  # No division by zero possible
+                T_temp[3, :] = [(c_p*(1-c_ks))/kappa[i], (s_p*(1-c_ks))/kappa[i], s_ks/kappa[i], 1]
 
-        T_base = T[ptsperseg[i] - 1, :].reshape(4,4)
+            g[i*numseg + j, :] = (T_base @ T_temp).reshape(1, 16)  # populate with transformation matrix for that point
+
+        T_base = g[i*numseg + ptsperseg[i] - 1, :].reshape(4, 4)  # lastmost point's transformation matrix is the new base
 
     return g
