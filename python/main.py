a9df805d750944a8879729285dbc8ba9a25c7fb3
dhan2325 <danihan2325@gmail.com>
2024-09-25 17:40:03 -0400

    path managed via poetry

--- python/main.py
+++ python/main.py
@@ -1,11 +1,5 @@
-import sys
-import os
-
 from plotter.main import plot_from_file
 from parser import parser
-
-# Add the project root directory to sys.path
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 if __name__ == "__main__":
     args = parser.parse_args()
