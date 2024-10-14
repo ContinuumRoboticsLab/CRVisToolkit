a95ede11112de5df31e3e4d529ccfb4a9c3d89fb
dhan2325 <danihan2325@gmail.com>
2024-10-14 13:51:51 -0400

    add 3-seg test

--- python/ik/tests/nr_tests.py
+++ python/ik/tests/nr_tests.py
@@ -5,7 +5,7 @@ Test Cases for the Newton-Rhapson Inverse Kinematics Solver
 
 2. a two segment extensible robot: tested for target R3 position and SE3 pose
 
-3. a two segment extensible robot, target pose is a position-pointing target
+3. a three-segment inextensible robot: tested for target R3 position and SE3 pose
 """
 
 from math import pi
@@ -128,8 +128,52 @@ def test_nr_2(plot, logger):
     return (se3_res, p3_res)
 
 
-def test_nr_3(logger):
-    pass
+def test_nr_3(plot, logger):
+    """
+    testing a three-segment inextensible robot
+    """
+    logger.info("**** Test Case 2: two-segment extensible CR****")
+
+    seg1 = ConstantCurvatureSegment(1 / 0.14, -0.8 * pi, 0.05)
+    seg2 = ConstantCurvatureSegment(1 / 0.06, 0.4 * pi, 0.03)
+    seg3 = ConstantCurvatureSegment(1 / 0.065, -pi, 0.035)
+    robot = ConstantCurvatureCR([seg1, seg2, seg3])
+
+    target_seg1 = ConstantCurvatureSegment(1 / 0.13, -pi, 0.05)
+    target_seg2 = ConstantCurvatureSegment(1 / 0.07, 0.35 * pi, 0.03)
+    target_seg3 = ConstantCurvatureSegment(1 / 0.04, -0.9 * pi, 0.035)
+    target_robot = ConstantCurvatureCR([target_seg1, target_seg2, target_seg3])
+    logger.info(
+        f"starting curvature is: {robot.state_vector()} \
+        yielding state\n {robot.pose_vector(robot.state_vector())}"
+    )
+
+    settings = NewtonRhapsonIkSettings()
+
+    target_pose = SE3IkTarget(target_robot.pose_vector())
+    target_position = P3IkTarget(target_robot.pose_vector())
+
+    logger.info(f"target pose: {target_pose.pose}")
+
+    se3_solver = NewtonRhapsonIkSolver(
+        robot, settings, robot.state_vector(), target_pose
+    )
+    p3_solver = NewtonRhapsonIkSolver(
+        robot, settings, robot.state_vector(), target_position
+    )
+
+    se3_res = _run_solver_test(target_robot, se3_solver, logger)
+    p3_res = _run_solver_test(target_robot, p3_solver, logger)
+
+    if plot:
+        logger.info("plotting position solution")
+        draw_tdcr(p3_solver.cr.as_discrete_curve(pts_per_seg=10))
+        logger.info("plotting pose solution")
+        draw_tdcr(se3_solver.cr.as_discrete_curve(pts_per_seg=10))
+        logger.info("plotting target robot")
+        draw_tdcr(target_robot.as_discrete_curve(pts_per_seg=10))
+
+    return (se3_res, p3_res)
 
 
 def run(plot=False, loglevel=logging.INFO):
@@ -138,3 +182,4 @@ def run(plot=False, loglevel=logging.INFO):
     logger = logging.getLogger(__name__)
     test_nr_1(plot, logger)
     test_nr_2(plot, logger)
+    test_nr_3(plot, logger)
